f6597e422453052d771c80a072b74558b3f9d02b
D3BaNaNa <videep.mannava@gmail.com>
2025-07-05 14:53:54 -0700

    Hopefully fixed tsa logo not showing

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export default function Logo({ className = '' }: { className?: string }) {
   return (
     <Image
-      src="/images/ncTSALogo.png"
+      src="/ncTSALogo.png"
       alt="TSA"
       width={48}
       height={48}
